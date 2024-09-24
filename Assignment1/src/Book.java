2b5148c5a6ad27bebaae7b8cf6a3c7893357ae11
TuanNguyen-VietIS <tuannv@vietis.com.vn>
2024-09-24 17:22:55 +0700

    push 1.3.2

--- Assignment1/src/Book.java
+++
@@ -1,6 +0,0 @@
-public class Book {
-    private String name;
-    private String author;
-    private int price;
-    private int quantity;
-}
--- Assignment1/src/Customer.java
+++
@@ -1,5 +0,0 @@
-public class Customer {
-    private String name;
-    private String address;
-    private String phoneNumber;
-}
